b4f8cda3030c11667d5a1d8017bbb575a9a20191
Rolf Veinø Sørensen <rolf@veinoe.dk>
2016-03-15 21:34:46 +0100

    fix error controller so it will get css theme from settings

--- MoM.Web/Controllers/ErrorController.cs
+++ MoM.Web/Controllers/ErrorController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.OptionsModel;
+using MoM.Web.Config;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,9 +8,17 @@ namespace MoM.Web.Controllers
 {
     public class ErrorController : Controller
     {
+        IOptions<Site> SiteSettings;
+
+        public ErrorController(IOptions<Site> siteSettings)
+        {
+            SiteSettings = siteSettings;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
+            var theme = SiteSettings.Value.Theme;
+            ViewData["CssPath"] = "css/" + theme.Module + "/" + theme.Selected + "/";
             return View();
         }
     }
