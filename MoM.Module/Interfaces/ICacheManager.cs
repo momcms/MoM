7298574bbf78cb71b502cd7062a88285e08f2b0f
Rolf Veinø Sørensen <rolf@veinoe.dk>
2016-04-21 23:09:26 +0200

    create basic cachemanager

---
+++ MoM.Module/Interfaces/ICacheManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+
+namespace MoM.Module.Interfaces
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+        void Set(string key, object data, TimeSpan timeout, CacheItemPriority priority);
+        void Remove(string key);
+    }
+}
---
+++ MoM.Module/Managers/CacheManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MoM.Module.Managers
+{
+    public class CacheManager
+    {
+        private IMemoryCache MemoryCache;
+
+        public CacheManager(MemoryCache memoryCache)
+        {
+            MemoryCache = memoryCache;
+        }
+
+        public T Get<T>(string key)
+        {
+            return (T)MemoryCache.Get(key);
+        }
+
+        public void Set(string key, object data, TimeSpan timeout, CacheItemPriority priority)
+        {
+            MemoryCache.Set(key, data, new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = timeout, Priority = priority });
+        }
+
+        public void Remove(string key)
+        {
+            MemoryCache.Remove(key);
+        }
+    }
+}
